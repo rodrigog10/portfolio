da1b623a52fa33aada82137d610643a939a4441e
Rodrigo Bispo <rodrigobisppo123@gmail.com>
2026-04-07 16:34:12 -0300

    troca da foto

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,13 +13,14 @@ export default function Header() {
                     <div className="flex ">
                         <div className="flex-col">
                             <img id="contato"
-                                src="/euportfolio.jpeg"
+                                src="/seriedade.jpeg"
                                 alt="Rodrigo"
                                 className=" my-4 self-end w-36 h-36 md:w-54 md:h-54 rounded-full object-cover shadow-lg
                                 transition-transform duration-300 hover:scale-105"
                             />
                         <h1 className="text-[2.7rem] md:text-[6rem] font-semibold md:font-medium ">
-                            Hi, <span className="  md:hidden"><br /></span>
+                            Hi, <span className="  md:hidden"> <br/>
+                        </span>
                             i'm{" "}
                             <span className="relative inline-block after:content-['']
                         after:absolute after:left-0 after:bottom-2 after:w-0 after:h-[3px]
